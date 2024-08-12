3bde43853a6711050b269e7a360bfd925ccada3b
Edward Lu <lued2018@gmail.com>
2024-08-12 18:39:39 -0400

    add depth peeling example

---
+++ apps/depth_peeling/src/depth_peeling.cpp
@@ -0,0 +1,336 @@
+#include <iostream>
+
+#include <args.hxx>
+
+#include <OpenGLApp.h>
+#include <Renderers/DepthPeelingRenderer.h>
+#include <SceneLoader.h>
+#include <Windowing/GLFWWindow.h>
+#include <GUI/ImGuiManager.h>
+
+#define TEXTURE_PREVIEW_SIZE 300
+
+int main(int argc, char** argv) {
+    Config config{};
+    config.title = "Depth Peeling";
+
+    args::ArgumentParser parser(config.title);
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+    args::ValueFlag<std::string> sizeIn(parser, "size", "Size of window", {'s', "size"}, "800x600");
+    args::ValueFlag<std::string> scenePathIn(parser, "scene", "Path to scene file", {'S', "scene"}, "../assets/scenes/sponza.json");
+    args::ValueFlag<bool> vsyncIn(parser, "vsync", "Enable VSync", {'v', "vsync"}, true);
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help) {
+        std::cout << parser;
+        return 0;
+    } catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    // parse size
+    std::string sizeStr = args::get(sizeIn);
+    size_t pos = sizeStr.find('x');
+    config.width = std::stoi(sizeStr.substr(0, pos));
+    config.height = std::stoi(sizeStr.substr(pos + 1));
+
+    config.enableVSync = args::get(vsyncIn);
+
+    std::string scenePath = args::get(scenePathIn);
+
+    auto window = std::make_shared<GLFWWindow>(config);
+    auto guiManager = std::make_shared<ImGuiManager>(window);
+
+    config.window = window;
+    config.guiManager = guiManager;
+
+    OpenGLApp app(config);
+    DepthPeelingRenderer renderer(config);
+
+    unsigned int screenWidth, screenHeight;
+    window->getSize(screenWidth, screenHeight);
+
+    Scene scene = Scene();
+    Camera camera = Camera(screenWidth, screenHeight);
+    SceneLoader loader = SceneLoader();
+    loader.loadScene(scenePath, scene, camera);
+
+    float exposure = 1.0f;
+    int shaderIndex = 0;
+    RenderStats renderStats;
+    guiManager->onRender([&](double now, double dt) {
+        static bool showFPS = true;
+        static bool showUI = true;
+        static bool showCaptureWindow = false;
+        static bool saveAsHDR = false;
+        static char fileNameBase[256] = "screenshot";
+
+        glm::vec2 winSize = glm::vec2(screenWidth, screenHeight);
+
+        ImGui::NewFrame();
+
+        unsigned int flags = 0;
+        ImGui::BeginMainMenuBar();
+        if (ImGui::BeginMenu("File")) {
+            if (ImGui::MenuItem("Exit", "ESC")) {
+                window->close();
+            }
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("View")) {
+            ImGui::MenuItem("FPS", 0, &showFPS);
+            ImGui::MenuItem("UI", 0, &showUI);
+            ImGui::MenuItem("Frame Capture", 0, &showCaptureWindow);
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+
+        if (showFPS) {
+            ImGui::SetNextWindowPos(ImVec2(10, 40), ImGuiCond_FirstUseEver);
+            flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar;
+            ImGui::Begin("", 0, flags);
+            ImGui::Text("%.1f FPS (%.3f ms/frame)", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        if (showUI) {
+            ImGui::SetNextWindowSize(ImVec2(600, 500), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowPos(ImVec2(10, 90), ImGuiCond_FirstUseEver);
+            ImGui::Begin(config.title.c_str(), &showUI);
+            ImGui::Text("OpenGL Version: %s", glGetString(GL_VERSION));
+            ImGui::Text("GPU: %s\n", glGetString(GL_RENDERER));
+
+            ImGui::Separator();
+
+            if (renderStats.trianglesDrawn < 100000)
+                ImGui::TextColored(ImVec4(0,1,0,1), "Total Triangles Drawn: %d", renderStats.trianglesDrawn);
+            else if (renderStats.trianglesDrawn < 500000)
+                ImGui::TextColored(ImVec4(1,1,0,1), "Total Triangles Drawn: %d", renderStats.trianglesDrawn);
+            else
+                ImGui::TextColored(ImVec4(1,0,0,1), "Total Triangles Drawn: %d", renderStats.trianglesDrawn);
+
+            if (renderStats.drawCalls < 200)
+                ImGui::TextColored(ImVec4(0,1,0,1), "Total Draw Calls: %d", renderStats.drawCalls);
+            else if (renderStats.drawCalls < 500)
+                ImGui::TextColored(ImVec4(1,1,0,1), "Total Draw Calls: %d", renderStats.drawCalls);
+            else
+                ImGui::TextColored(ImVec4(1,0,0,1), "Total Draw Calls: %d", renderStats.drawCalls);
+
+            ImGui::Separator();
+
+            glm::vec3 position = camera.getPosition();
+            if (ImGui::InputFloat3("Camera Position", (float*)&position)) {
+                camera.setPosition(position);
+            }
+            glm::vec3 rotation = camera.getRotationEuler();
+            if (ImGui::InputFloat3("Camera Rotation", (float*)&rotation)) {
+                camera.setRotationEuler(rotation);
+            }
+            ImGui::SliderFloat("Movement Speed", &camera.movementSpeed, 0.1f, 20.0f);
+
+            ImGui::Separator();
+
+            if (ImGui::CollapsingHeader("Directional Light Settings")) {
+                ImGui::TextColored(ImVec4(1,1,1,1), "Directional Light Settings");
+                ImGui::ColorEdit3("Color", (float*)&scene.directionalLight->color);
+                ImGui::SliderFloat("Strength", &scene.directionalLight->intensity, 0.1f, 100.0f);
+                ImGui::SliderFloat3("Direction", (float*)&scene.directionalLight->direction, -5.0f, 5.0f);
+            }
+
+            ImGui::Separator();
+
+            if (ImGui::CollapsingHeader("Post Processing Settings")) {
+                ImGui::SliderFloat("Exposure", &exposure, 0.1f, 5.0f);
+                ImGui::RadioButton("Show Color", &shaderIndex, 0);
+                ImGui::RadioButton("Show Depth", &shaderIndex, 1);
+                ImGui::RadioButton("Show Positions", &shaderIndex, 2);
+                ImGui::RadioButton("Show Normals", &shaderIndex, 3);
+                ImGui::RadioButton("Show Primative IDs", &shaderIndex, 4);
+            }
+
+            ImGui::End();
+        }
+
+        if (showCaptureWindow) {
+            ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowPos(ImVec2(winSize.x * 0.4, 90), ImGuiCond_FirstUseEver);
+            ImGui::Begin("Frame Capture", &showCaptureWindow);
+
+            ImGui::Text("Base File Name:");
+            ImGui::InputText("##base file name", fileNameBase, IM_ARRAYSIZE(fileNameBase));
+            std::string fileName = std::string(fileNameBase) + "." + std::to_string(static_cast<int>(window->getTime() * 1000.0f));
+
+            ImGui::Checkbox("Save as HDR", &saveAsHDR);
+
+            ImGui::Separator();
+
+            if (ImGui::Button("Capture Current Frame")) {
+                if (saveAsHDR) {
+                    renderer.gBuffer.saveColorAsHDR(fileName + ".hdr");
+                }
+                else {
+                    renderer.gBuffer.saveColorAsPNG(fileName + ".png");
+                }
+            }
+
+            ImGui::End();
+        }
+
+        flags = ImGuiWindowFlags_AlwaysAutoResize;
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 4 * TEXTURE_PREVIEW_SIZE - 120, 40), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 0 Color", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[0]->colorBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 3 * TEXTURE_PREVIEW_SIZE - 90, 40), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 1 Color", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[1]->colorBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 2 * TEXTURE_PREVIEW_SIZE - 60, 40), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 2 Color", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[2]->colorBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - TEXTURE_PREVIEW_SIZE - 30, 40), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 3 Color", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[3]->colorBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 4 * TEXTURE_PREVIEW_SIZE - 120, 40 + 60 + TEXTURE_PREVIEW_SIZE), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 0 Depth", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[0]->depthBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 3 * TEXTURE_PREVIEW_SIZE - 90, 40 + 60 + TEXTURE_PREVIEW_SIZE), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 1 Depth", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[1]->depthBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - 2 * TEXTURE_PREVIEW_SIZE - 60, 40 + 60 + TEXTURE_PREVIEW_SIZE), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 2 Depth", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[2]->depthBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(screenWidth - TEXTURE_PREVIEW_SIZE - 30, 40 + 60 + TEXTURE_PREVIEW_SIZE), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Layer 3 Depth", 0, flags);
+        ImGui::Image((void*)(intptr_t)(renderer.peelingLayers[3]->depthBuffer.ID), ImVec2(TEXTURE_PREVIEW_SIZE, TEXTURE_PREVIEW_SIZE), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::End();
+    });
+
+    app.onResize([&](unsigned int width, unsigned int height) {
+        screenWidth = width;
+        screenHeight = height;
+
+        renderer.resize(width, height);
+
+        camera.aspect = (float)screenWidth / (float)screenHeight;
+        camera.updateProjectionMatrix();
+    });
+
+    // shaders
+    Shader showColorShader({
+        .vertexCodePath = "../shaders/postprocessing/postprocess.vert",
+        .fragmentCodePath = "../shaders/postprocessing/displayColor.frag"
+    });
+
+    Shader showDepthShader({
+        .vertexCodePath = "../shaders/postprocessing/postprocess.vert",
+        .fragmentCodePath = "../shaders/postprocessing/displayDepth.frag"
+    });
+
+    Shader showPositionShader({
+        .vertexCodePath = "../shaders/postprocessing/postprocess.vert",
+        .fragmentCodePath = "../shaders/postprocessing/displayPositions.frag"
+    });
+
+    Shader showNormalShader({
+        .vertexCodePath = "../shaders/postprocessing/postprocess.vert",
+        .fragmentCodePath = "../shaders/postprocessing/displayNormals.frag"
+    });
+
+    Shader showIDShader({
+        .vertexCodePath = "../shaders/postprocessing/postprocess.vert",
+        .fragmentCodePath = "../shaders/postprocessing/displayIDs.frag"
+    });
+
+    app.onRender([&](double now, double dt) {
+        // handle mouse input
+        if (!(ImGui::GetIO().WantCaptureKeyboard || ImGui::GetIO().WantCaptureMouse)) {
+            auto mouseButtons = window->getMouseButtons();
+            window->setMouseCursor(!mouseButtons.LEFT_PRESSED);
+            static bool dragging = false;
+            static bool prevMouseLeftPressed = false;
+            static float lastX = screenWidth / 2.0;
+            static float lastY = screenHeight / 2.0;
+            if (!prevMouseLeftPressed && mouseButtons.LEFT_PRESSED) {
+                dragging = true;
+                prevMouseLeftPressed = true;
+
+                auto cursorPos = window->getCursorPos();
+                lastX = static_cast<float>(cursorPos.x);
+                lastY = static_cast<float>(cursorPos.y);
+            }
+            if (prevMouseLeftPressed && !mouseButtons.LEFT_PRESSED) {
+                dragging = false;
+                prevMouseLeftPressed = false;
+            }
+            if (dragging) {
+                auto cursorPos = window->getCursorPos();
+                float xpos = static_cast<float>(cursorPos.x);
+                float ypos = static_cast<float>(cursorPos.y);
+
+                float xoffset = xpos - lastX;
+                float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+                lastX = xpos;
+                lastY = ypos;
+
+                camera.processMouseMovement(xoffset, yoffset, true);
+            }
+        }
+
+        // handle keyboard input
+        auto keys = window->getKeys();
+        camera.processKeyboard(keys, dt);
+        if (keys.ESC_PRESSED) {
+            window->close();
+        }
+
+        // render all objects in scene
+        renderStats = renderer.drawObjects(scene, camera);
+
+        // render to screen
+        if (shaderIndex == 1) {
+            showDepthShader.bind();
+            showDepthShader.setFloat("near", camera.near);
+            showDepthShader.setFloat("far", camera.far);
+            renderer.drawToScreen(showDepthShader);
+        }
+        else if (shaderIndex == 2) {
+            showPositionShader.bind();
+            renderer.drawToScreen(showPositionShader);
+        }
+        else if (shaderIndex == 3) {
+            showNormalShader.bind();
+            renderer.drawToScreen(showNormalShader);
+        }
+        else if (shaderIndex == 4) {
+            showIDShader.bind();
+            renderer.drawToScreen(showIDShader);
+        }
+        else {
+            showColorShader.bind();
+            showColorShader.setFloat("exposure", exposure);
+            renderer.drawToScreen(showColorShader);
+        }
+    });
+
+    // run app loop (blocking)
+    app.run();
+
+    return 0;
+}
